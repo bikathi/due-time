986c9f942b037c8209d0af03d77277fe37ffd87d
bikathi-martin <bikathimartin@outlook.com>
2025-06-12 05:58:54 +0300

    Create and style form to create a TODO

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -2,42 +2,52 @@ import "./index.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const appElement = document.getElementById("app")!;
-  const link = "https://github.com/bikathi/ts-chrome-ext-starter";
+  const link = "https://github.com/bikathi/due-time";
 
   // Create the main element
   const mainElement = document.createElement("main");
 
   // Create the title element
   const h3Element = document.createElement("h3");
-  h3Element.className = "popup-h3-title";
-  h3Element.textContent = "Popup Page";
+  h3Element.textContent = "Add a TODO";
 
-  // Create the counter element
-  const divElement = document.createElement("div");
-  divElement.className = "calc-area";
-  const minusButton = document.createElement("button");
-  minusButton.className = "calc-btns";
-  minusButton.textContent = "-";
-  const countLabel = document.createElement("label");
-  countLabel.className = "calc-label";
-  countLabel.textContent = "0";
-  const addButton = document.createElement("button");
-  addButton.className = "calc-btns";
-  addButton.textContent = "+";
-  divElement.appendChild(minusButton);
-  divElement.appendChild(countLabel);
-  divElement.appendChild(addButton);
+  // create the form element
+  const formElement = document.createElement("form");
 
-  // Create the link element
+  // title input
+  const titleInput = document.createElement("input");
+  titleInput.setAttribute("type", "text");
+  titleInput.className = "form-input";
+  titleInput.setAttribute("maxLength", "50");
+  titleInput.placeholder = "Remind me to feed my cat...";
+  titleInput.setAttribute("required", "true");
+
+  // date-time-selector input
+  const dateTimeSelectorInput = document.createElement("input");
+  dateTimeSelectorInput.setAttribute("type", "datetime-local");
+  dateTimeSelectorInput.className = "form-input";
+  titleInput.setAttribute("required", "true");
+
+  // submit button
+  const formSubmitButton = document.createElement("button");
+  formSubmitButton.id = "form-submit-button";
+  formSubmitButton.setAttribute("type", "submit");
+  formSubmitButton.innerText = "Save";
+
+  formElement.appendChild(titleInput);
+  formElement.appendChild(dateTimeSelectorInput);
+  formElement.appendChild(formSubmitButton);
+
+  // Create the link element to the Github
   const aElement = document.createElement("a");
   aElement.href = link;
   aElement.className = "link-color";
   aElement.target = "_blank";
-  aElement.textContent = "executed from starter template";
+  aElement.textContent = "view code on Github";
 
   // Append all elements to the main element
   mainElement.appendChild(h3Element);
-  mainElement.appendChild(divElement);
+  mainElement.appendChild(formElement);
   mainElement.appendChild(aElement);
 
   // Append the main element to the page
@@ -46,26 +56,26 @@ document.addEventListener("DOMContentLoaded", () => {
   let count = 0;
 
   // Get the count value from Chrome storage
-  chrome.storage.sync.get(["count"], function (result) {
-    count = result.count || 0;
-    countLabel.textContent = `${count}`;
-  });
+  // chrome.storage.sync.get(["count"], function (result) {
+  //   count = result.count || 0;
+  //   countLabel.textContent = `${count}`;
+  // });
 
   // Decrement the count
-  minusButton.addEventListener("click", function () {
-    if (count > 0) {
-      count--;
-      countLabel.textContent = `${count}`;
-      chrome.storage.sync.set({ count });
-      chrome.runtime.sendMessage({ type: "COUNT", count });
-    }
-  });
+  // minusButton.addEventListener("click", function () {
+  //   if (count > 0) {
+  //     count--;
+  //     countLabel.textContent = `${count}`;
+  //     chrome.storage.sync.set({ count });
+  //     chrome.runtime.sendMessage({ type: "COUNT", count });
+  //   }
+  // });
 
   // Increment the count
-  addButton.addEventListener("click", function () {
-    count++;
-    countLabel.textContent = `${count}`;
-    chrome.storage.sync.set({ count });
-    chrome.runtime.sendMessage({ type: "COUNT", count });
-  });
+  // addButton.addEventListener("click", function () {
+  //   count++;
+  //   countLabel.textContent = `${count}`;
+  //   chrome.storage.sync.set({ count });
+  //   chrome.runtime.sendMessage({ type: "COUNT", count });
+  // });
 });
